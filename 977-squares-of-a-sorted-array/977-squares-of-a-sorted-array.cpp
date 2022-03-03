1f8656061d65b5c7f6ac1a78da5ab1f64fbe96ba
Apurva115 <43859736+Apurva115@users.noreply.github.com>
2022-03-03 22:59:43 +0530

    Time: 19 ms (99.45%), Space: 25.8 MB (81.95%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,70 +1,33 @@
 class Solution {
 public:
-    vector<int> merge(vector<int>& arr1, vector<int>& arr2)
-    {
-        vector<int> ans;
-        int i=0;
-        int j=0;
-        
-        while(i<arr1.size() && j<arr2.size())
-        {
-            if(arr1[i]<=arr2[j])
-            {
-                ans.push_back(arr1[i]);
-                i++;
-                
-            }
-            else
-            {
-                ans.push_back(arr2[j]);
-                j++;
-            }
-        
-        }
-        while(i<arr1.size())
-        {
-            ans.push_back(arr1[i]);
-            i++;
-        }
-        while(j<arr2.size())
-        {
-            ans.push_back(arr2[j]);
-            j++;
-        }
-        return ans;
-    }
     vector<int> sortedSquares(vector<int>& nums) {
-        vector<int> arr1;
-        vector<int> arr2;
-        for(auto x: nums)
+        vector<int> ans;
+        
+        if(nums.size()==1)
         {
-            if(x>=0)
+            ans.push_back(nums[0]*nums[0]);
+            return ans;
+        }
+        ans.assign(nums.size(),0);
+        int left = 0;
+        int k = nums.size()-1;
+        int right = nums.size()-1;
+        while(left <=right)
+        {
+            if(nums[left]*nums[left]> nums[right]*nums[right])
             {
-                arr1.push_back(x*x);
+                ans[k] = nums[left]*nums[left];
+                left++;
+                k--;
             }
             else
             {
-                arr2.push_back(x*x);
+                ans[k]= nums[right]*nums[right];
+                k--;
+                right--;
             }
+            
         }
-        if(arr2.size()==0)
-        {
-            return arr1;
-        }
-        reverse(arr2.begin(), arr2.end());
-        if(arr1.size()==0)
-        {
-            return arr2;
-        }
-      
-
-        vector<int> ans;
-        ans= merge(arr1, arr2);
         return ans;
-        
-        
-        
-
-        
     }
 };
