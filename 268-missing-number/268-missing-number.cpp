28df59344f15e83776c0812b1cece7e70ef3f5c4
Apurva115 <43859736+Apurva115@users.noreply.github.com>
2022-03-09 22:01:25 +0530

    Time: 16 ms (93.21%), Space: 18 MB (65.27%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int len = nums.size()+1;
-        int ans;
-        vector<int> frequency(len, 0);
-        for(int i=0; i<nums.size(); i++)
+        int res =0;
+        int i;
+        for( i=0; i< nums.size(); i++)
         {
-            frequency[nums[i]]++;
-            
+            res^= nums[i];
+            res^=i;
         }
-        for(int i=0; i<len; i++)
-        {
-            if(frequency[i]== 0)
-            {
-                ans = i;
-                break;
-            }
-        }
-        return ans;
+        res^=i;
+        return res;
+
 
         
     }
